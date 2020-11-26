c1effc44d110109f16f9e47fab858f9bc02419f6
ghar-wa <74982659+ghar-wa@users.noreply.github.com>
2020-11-26 21:23:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(process.env.PORT || 3000,()=>console.log("server connected"));
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
